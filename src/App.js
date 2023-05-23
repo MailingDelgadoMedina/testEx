fd5c027c18be545eaf4d1590eebda0b81cb8c08b
Tenzin Paldon <tenzinpaldon@Tenzins-iMac.lan>
2023-05-22 20:01:04 -0400

    added background

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div >
-       <h1 className="text-3xl font-bold underline">
+       <h1 className=" bg-orange-400 text-3xl font-bold underline">
       Hello world!
     </h1>
     </div>
