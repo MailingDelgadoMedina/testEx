4d506b9fe1918ab19da9d8c4e951b6d1c7ce29d8
MailingDelgadoMedina <mailing.delgado.scitexas@gmail.com>
2023-05-22 20:16:36 -0400

    close #1, I finished the NavBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div >
+      <Navbar/>
        <h1 className=" bg-orange-400 text-3xl font-bold underline">
       Hello world!
     </h1>
