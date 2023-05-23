92105ac7b8b1b756b5d8392df96a71a795639093
MailingDelgadoMedina <mailing.delgado.scitexas@gmail.com>
2023-05-22 20:50:15 -0400

    did footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './Navbar';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
        <h1 className=" bg-orange-400 text-3xl font-bold underline">
       Hello world!
     </h1>
+    <Footer/>
     </div>
   );
 }   
